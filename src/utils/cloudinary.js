07717d84e2fa65f7555a67e9f71c7a9642512aa8
Prakhar Shukla <prakhar.shukla@benosupport.com>
2026-06-22 13:00:05 +0530

    Worked on Cloudinary and Multer Configuartion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import {v2 as clodinary} from "clodinary"
+import { log } from "console";
+import fs from "fs"
+
+clodinary.config({
+    cloud_name: process.env.clodinary.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if (!localFilePath) return null
+        //upload the file on clodinary
+        clodinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        } )
+        // file has been uploaded successfully
+        console.log("File is uploaded on clodinary",
+        response.url);
+        return response;
+            
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved
+        //  temporary file as the upload opertion got failed
+        return null;
+    }
+}
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
