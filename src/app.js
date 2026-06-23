d6676b5cec1f72635830c6b05a9e740e2016b6cc
Prakhar Shukla <prakhar.shukla@benosupport.com>
2026-06-23 12:32:59 +0530

    fixed routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,4 +14,13 @@ app.use(express.static("public"))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export {app}
+// routes import 
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declartion
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export {app};
