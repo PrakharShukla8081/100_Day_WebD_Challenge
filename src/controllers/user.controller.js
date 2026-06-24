df55eaccb9953f66dac028ad45d70d3db87c7e3b
Prakhar Shukla <prakhar.shukla@benosupport.com>
2026-06-24 18:16:49 +0530

    worked on route and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,20 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 
-const registerUser = asyncHandler(async (requestAnimationFrame, res)=>{
-    res.status(200).json({
-        messsage: "ok"
-    })
+const registerUser = asyncHandler(async (req, res)=>{
+   // get user details from frontend
+   // validation - not empty
+   // check if user already exists: username, email
+   // check for images , check for avtar
+   // upload them to cloudnary, avtar
+   // create user object - create entry in db
+   // remove password and refresh token from field
+   // check for user creation 
+   // refresh token 
+   
+     const {fullname,email, username, password } = req.body
+        console.log("Email============================= :", email);
+        
 })
 
 export {registerUser}
